3f8b187fd9895b875215f7e851d626eba4873812
Joanner Pena <joanner.c1006@hotmail.com>
2024-05-04 20:41:41 -0400

    Set Coming soon

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,12 @@
-import React from "react";
-import data from "../../../data/data";
-import Navigation from "../Navigation/Navigation";
+import React from 'react';
+import data from '../../../data/data';
+import Navigation from '../Navigation/Navigation';
 
 function Header() {
   const { sitename, sitetagline, siteurl } = data;
   return (
     <>
-    <section className="container mx-auto p-4 text-center">
-      <Navigation />      
-    </section>
+      <section className="container mx-auto p-4 text-center"></section>
       <header className="z-10 w-full max-w-5xl items-center justify-between text-sm ">
         <div className=" bottom-0 left-0 flex h-30 md:h-48 w-full items-end justify-center  lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
