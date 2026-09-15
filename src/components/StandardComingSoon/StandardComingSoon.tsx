df97056b340c830c35ca61f1cc2af51e8be6425e
Joanner Pena <joanner.c1006@hotmail.com>
2026-09-15 00:41:39 -0400

    fixed newsletter failing

--- src/components/StandardComingSoon/StandardComingSoon.tsx
+++ src/components/StandardComingSoon/StandardComingSoon.tsx
@@ -1,11 +1,9 @@
-
-import { Inter } from "next/font/google";
-import data from "../../../data/data";
-const inter = Inter({ subsets: ["latin"] });
-import SubscribeForm from "../SubscribeForm/SubscribeForm";
-import Footer from "../Footer/Footer";
-import Header from "../Header/Header";
-import ComingSoonContent from "../ComingSoonContent/ComingSoonContent";
+import { Inter } from 'next/font/google';
+import data from '../../../data/data';
+const inter = Inter({ subsets: ['latin'] });
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+import ComingSoonContent from '../ComingSoonContent/ComingSoonContent';
 
 export default function StandardComingSoon() {
   const {
@@ -20,13 +18,11 @@ export default function StandardComingSoon() {
     title,
   } = data;
 
-
   return (
     <>
       <main className="flex min-h-screen flex-col items-center justify-between p-5 lg:p-12">
         <Header />
         <ComingSoonContent />
-        <SubscribeForm />
         <Footer />
       </main>
     </>
